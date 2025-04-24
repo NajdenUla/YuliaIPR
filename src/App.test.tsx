ed45051cca27fb954e55a73c71db33ce6f5508cf
ÐNajdenUla <ulianajden43@gmail.com>
2025-04-25 01:46:06 +0300

    тралалÐ »о тралала

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders catalog heading', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headingElement = await screen.findByRole('heading', {
+    name: /каталог фильмов/i,
+  });
+  expect(headingElement).toBeInTheDocument();
 });
